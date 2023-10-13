173a2ae83bc0d4a95ef3e1747f3170d0e0c5718d
Monk <aadityaura@gmail.com>
2023-10-14 02:01:25 +0530

    Update update_readme.py

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -1,28 +1,17 @@
 import json
 
-# Read the metadata.json file
-with open("metadata.json", "r") as f:
+# Load metadata.json
+with open('metadata.json', 'r') as f:
     data = json.load(f)
 
-# Create a markdown table header
-table = "| Name | Source Name | Source URL |\n"
-table += "|------|-------------|------------|\n"
-
-# Populate the table rows with the 'name' and 'source' information
+# Generate table
+table = "| Name | Source |\n| --- | --- |\n"
 for item in data:
-    name = item["name"]
-    source_name = item["source"]["name"]
-    source_url = item["source"]["url"]
-    table += f"| {name} | {source_name} | [Link]({source_url}) |\n"
+    name = item['name']
+    source_name = item['source']['name']
+    source_url = item['source']['url']
+    table += f"| {name} | [{source_name}]({source_url}) |\n"
 
-# Update the README.md file
-readme_path = "README.md"
-with open(readme_path, "r") as file:
-    readme_content = file.readlines()
-
-insert_index = readme_content.index("# Models\n") + 1
-readme_content = readme_content[:insert_index] + [table] + readme_content[insert_index:]
-
-# Write the updated content back to README.md
-with open(readme_path, "w") as file:
-    file.writelines(readme_content)
+# Write to README.md
+with open('README.md', 'w') as f:
+    f.write(table)
